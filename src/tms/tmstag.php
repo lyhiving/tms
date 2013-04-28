410c40c7ea835b3819739a7d61b639e7955e4d7d
tinyhill <tinyhill@163.com>
2013-04-28 14:00:31 +0800

    JSON_PRETTY_PRINT

--- src/tms/tmstag.php
+++ src/tms/tmstag.php
@@ -129,6 +129,51 @@ if (!function_exists('_tms_io')) {
 }
 
 /**
+ * 将 JSON 字符串转换为可读格式
+ * @param string $json
+ * @return string
+ */
+if (!function_exists('_tms_format')) {
+
+	function _tms_format ($json) {
+
+		$result = '';
+		$pos = 0;
+		$strLen = strlen($json);
+		$indentStr = '	';
+		$newLine = "\n";
+		$prevChar = '';
+		$outOfQuotes = true;
+
+		for ($i = 0; $i <= $strLen; $i++) {
+			$char = substr($json, $i, 1);
+			if ($char == '"' && $prevChar != '\\') {
+				$outOfQuotes = !$outOfQuotes;
+			} else if (($char == '}' || $char == ']') && $outOfQuotes) {
+				$result .= $newLine;
+				$pos--;
+				for ($j = 0; $j < $pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
+			$result .= $char;
+			if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+				$result .= $newLine;
+				if ($char == '{' || $char == '[') {
+					$pos++;
+				}
+				for ($j = 0; $j < $pos; $j++) {
+					$result .= $indentStr;
+				}
+			}
+			$prevChar = $char;
+		}
+		return $result;
+	}
+
+}
+
+/**
  * 导入标签数据文件
  * @param string $filename
  * @return void
